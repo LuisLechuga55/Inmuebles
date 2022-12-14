fff7b9c6d3631cc3ac0a649c570907426d81c3a2
Luis Lechuga <luislechu55@gmail.com>
2022-12-14 17:13:16 -0600

    feat: register and login validator costumer

---
+++ api/middlewares/registerCostumValidator.js
@@ -0,0 +1,23 @@
+import joi from 'joi';
+
+const costumerSchema = joi.object({
+  name: joi.string().required(),
+  lastName: joi.string().required(),
+  birthday: joi.date().required(),
+  phone: joi.string().required(),
+  email: joi.string().required(),
+  password: joi.string().required(),
+  addres: joi.string(),
+  country: joi.string().required(),
+  city: joi.string().required(),
+  zipCode: joi.string().required(),
+});
+
+export default async (req, res, next) => {
+  try {
+    await costumerSchema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+};
