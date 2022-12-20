8f30d2e1c75d3e8a97c15c214536ad98730004ca
Luis Lechuga <luislechu55@gmail.com>
2022-12-20 16:49:57 -0600

    change image controller

--- api/routes/imageRoutes.js
+++ api/routes/imageRoutes.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import multer from 'multer';
+import fs from 'fs'
 import { imageController } from '../controllers/index.js';
 import { Image } from '../models/index.js';
 
 const router = express.Router();
 
-const Storage = multer.diskStorage({
-  destination: 'uploads',
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, 'uploads');
+  },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
-  },
+  }
 });
 
-const upload = multer({
-  storage: Storage
-}).single('testImage')
+const upload = multer({ storage: storage })
 
-router.post('/upload', (req, res) => {
+/* router.post('/upload', (req, res) => {
   upload(req, res, (err) => {
     if (err) {
       console.log(err);
@@ -29,11 +30,32 @@ router.post('/upload', (req, res) => {
           contentType: 'image/png',
         }
       });
-      newImage.save().then(() => res.send('Successfully uploaded'))
-        .catch((err) => console.log(err));
+      newImage.save()
+        .then(() => 
+          res.send('Successfully uploaded'))
+        .catch((err) =>
+          console.log(err));
     }
     console.log('Image uploaded');
   });
+}); */
+
+router.post('/', upload.single('testImage'), (req, res) => {
+  const saveImage = new Image({
+    name: req.body.name,
+    image: {
+      data: fs.readFileSync('uploads/' + req.file.filename),
+      contentType: 'image/png',
+    }
+  });
+  saveImage.save()
+    .then(() => {
+      res.send('Successfully uploaded')
+      console.log('Image saved')
+    })
+    .catch((err) => {
+      console.log(err, 'Image not saved')
+    });
 });
 
 router
